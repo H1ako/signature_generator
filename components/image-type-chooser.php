d0c81c76fa80e7076a6a852eca72ad22db8ee209
h1ako <nikita-sobolev-wd@yandex.ru>
2023-05-21 00:20:28 +0400

    fixed bugs

--- components/image-type-chooser.php
+++ components/image-type-chooser.php
@@ -1,7 +1,7 @@
 <?php global $SITE_URL; ?>
 <dialog class="image-type-chooser" id="image-type-chooser">
   <div class="image-type-chooser__content">
-    <a href="" download="onlinesignatures.net.png" class="content__png-link" png-link>Png</a>
-    <a href="" download="onlinesignatures.net.jpg" class="content__jpg-link" jpg-link>Jpg</a>
+    <a href="#" download="onlinesignatures.net.png" class="content__png-link" png-link>Png</a>
+    <a href="#" download="onlinesignatures.net.jpg" class="content__jpg-link" jpg-link>Jpg</a>
   </div>
 </dialog>
